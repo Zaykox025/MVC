9f1964c4103a472b13ca0fa9f4d944cf9e8cc706
Zaykox025 <89661826+Zaykox025@users.noreply.github.com>
2021-11-04 15:11:14 -0500

    Add files via upload

---
+++ TRAIN SMART/FORMULARIO INICIO DE SESIÓN/Formulario de inicio/init.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/styleform.css">
+    <link rel="stylesheet" href="style/style2.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="com" center><h1>Bienvenidos</h1></div>
+    <div class="boot">
+<form method="post" action="" name="signin-form">
+    <div class="form-element">
+        <label>Username</label>
+        <input type="text" name="username" pattern="[a-zA-Z0-9]+" required />
+    </div>
+    <div class="form-element">
+        <label>Password</label>
+        <input type="password" name="password" required />
+    </div>
+    <button type="submit" name="login" value="login">Log In</button>
+</form>
+<marquee scrollamount="50"  behavior="" direction="right">
+    <img src="image/gif2.gif" alt="" width="30%" height="10%" >
+<marquee scrollamount="50"  behavior="" direction="right">
+    <img src="image/gif1.gif" alt="" width="10%" height="10%" >
+</marquee>
+</div>
+</body>
+</html>
